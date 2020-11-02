cf12e61d4d08c7a426c56d80a360c29980a39f47
MohammadAwaisJavaid <awais.javaid17@gmail.com>
2020-11-02 14:33:03 +0500

    lists in react

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
 import './index.css';
+const lakeList = [
+  "Echo Lake", 
+  "Maud Lake", 
+  "Cascade Lake"
+]
+function App(props){
+  return( <div>
+    <ul>
+      {props.lakes.map(lake=>(
+        <li>{lake}</li>
+      ))}
+    </ul>
+</div>);
+
+}
 
 ReactDOM.render(
-  <App />,
+  <App lakes = {lakeList}/>
+  ,
   document.getElementById('root')
 );
